912c76fbd9bd227c9f70270865c5583cf75dbaa0
Nirjhar Roy <nirjharr05@gmail.com>
2021-04-16 15:57:15 +0530

    image

--- socially-client/src/components/Profile.js
+++ socially-client/src/components/Profile.js
@@ -5,18 +5,19 @@ import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 //REDUX
 import { connect } from 'react-redux';
+import { logoutUser, uploadImage } from '../redux/actions/userActions';
 //MUI
 import Button from '@material-ui/core/Button';
 import MuiLink from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 //Icons
 import LocationOn from '@material-ui/icons/LocationOn';
 import LinkIcon from '@material-ui/icons/Link';
 import CalendarToday from '@material-ui/icons/CalendarToday';
 import EditIcon from '@material-ui/icons/Edit';
-
 
 const styles = (theme) => ({
     paper: {
@@ -64,8 +65,7 @@ const styles = (theme) => ({
         '& a': {
           margin: '20px 10px'
         }
-      }
-    
+      }    
 });
 
 
@@ -73,6 +73,9 @@ class Profile extends Component {
   handleImageChange = (event) => {
     const image = event.target.files[0];
     //send to server
+    const formData = new FormData();
+    formData.append('image', image, image.name);
+    this.props.uploadImage(formData);
   };
   handleEditPicture = () => {
     const fileInput = document.getElementById('imageInput');
@@ -106,9 +109,11 @@ class Profile extends Component {
                           hidden="hidden"
                           onChange={this.handleImageChange}
                         />
+                        <Tooltip title="Edit profile picture" placement="top">
                         <IconButton onClick={this.handleEditPicture} className="button">
                           <EditIcon color="primary"/>
                         </IconButton>
+                        </Tooltip>
                     </div>
                     <hr/> 
                     <div className="profile-details">
@@ -163,9 +168,13 @@ const mapStateToProps = (state) => ({
     user: state.user
 });
 
+const mapActionsToProps = { logoutUser, uploadImage };
+
 Profile.protoTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    uploadImage: PropTypes.func.isRequired,
     user: PropTypes.object.isRequired,
     classes: PropTypes.object.isRequired
-}
+};
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile))
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile))
