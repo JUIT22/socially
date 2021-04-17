6bbe6f17dd4891dfa080231b32098a3254a7109e
jscode <jscslg27@gmail.com>
2021-04-17 16:13:38 +0530

    dependencies updated

--- socially-client/src/components/Navbar.js
+++ socially-client/src/components/Navbar.js
@@ -3,18 +3,18 @@ import {Link} from 'react-router-dom';
 
 //MUI stuff(material UI)
 import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/ToolBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
 export class Navbar extends Component {
     render() {
         return (
             <AppBar>
-                <ToolBar className="nav-container">
+                <Toolbar className="nav-container">
                     <Button color="inherit" component={Link} to="/login">Login</Button>
                     <Button color="inherit" component={Link} to="/">Home</Button>
                     <Button color="inherit" component={Link} to="/signup">Signup</Button>
-                </ToolBar>
+                </Toolbar>
             </AppBar>
         )
     }
